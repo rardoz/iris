e4ce19cf03e314959a9cd211ffcaf156fc906285
rardoz <robert@zardozcs.com>
2017-04-20 14:35:36 -0700

    cleanup

--- bin/run.js
+++ bin/run.js
@@ -1,22 +1,19 @@
 'use strict';
 
-const slackClient = require('../server/slackClient');
-const service = require('../server/service');
-const http = require('http');
-const server = http.createServer(service);
+const 
+    slackClient = require('../server/slackClient'),
+    service = require('../server/service'),
+    http = require('http'),
+    server = http.createServer(service),
+    witToken = 'WIT_API_TOKEN',
+    witClient = require('../server/witClient')(witToken),
+    slackToken = 'SLACK_API_TOKEN',
+    slackLogLevel = 'verbose',
+    serviceRegistry = service.get('serviceRegistry'),
+    rtm = slackClient.init(slackToken, slackLogLevel, witClient, serviceRegistry)
 
-const witToken = 'WIT_API_TOKEN';
-const witClient = require('../server/witClient')(witToken);
+rtm.start()
 
-const slackToken = 'SLACK_API_TOKEN';
-const slackLogLevel = 'verbose';
+slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000))
 
-const serviceRegistry = service.get('serviceRegistry');
-const rtm = slackClient.init(slackToken, slackLogLevel, witClient, serviceRegistry);
-rtm.start();
-
-slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
-
-server.on('listening', function() {
-    console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
-});
+server.on('listening', () => console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`))
